83ca9cd006be3eec0e266e6d5d664687cf13215e
srijit.nair@databricks.com <srijit.nair@databricks.com>
2023-05-25 15:28:07 +0000

    added a comment

--- srijit_mlops_stack_demo/training/notebooks/TrainWithFeatureStore.py
+++ srijit_mlops_stack_demo/training/notebooks/TrainWithFeatureStore.py
@@ -15,8 +15,8 @@
 
 
 # COMMAND ----------
-# DBTITLE 1, Notebook arguments
 
+# DBTITLE 1, Notebook arguments
 # List of input args needed to run this notebook as a job.
 # Provide them via DB widgets or notebook arguments.
 
@@ -54,21 +54,22 @@ dbutils.widgets.text(
 )
 
 # COMMAND ----------
-# DBTITLE 1,Define input and output variables
 
+# DBTITLE 1,Define input and output variables
 input_table_path = dbutils.widgets.get("training_data_path")
 experiment_name = dbutils.widgets.get("experiment_name")
 model_name = dbutils.widgets.get("model_name")
 
 # COMMAND ----------
+
 # DBTITLE 1, Set experiment
 import mlflow
 
 mlflow.set_experiment(experiment_name)
 
 # COMMAND ----------
-# DBTITLE 1, Load raw data
 
+# DBTITLE 1, Load raw data
 raw_data = spark.read.format("delta").load(input_table_path)
 display(raw_data)
 
@@ -122,7 +123,7 @@ def rounded_taxi_data(taxi_data_df):
     taxi_data_df.createOrReplaceTempView("taxi_data")
     return taxi_data_df
 
-
+#adding a comment
 def get_latest_model_version(model_name):
     latest_version = 1
     mlflow_client = MlflowClient()
@@ -210,8 +211,8 @@ display(training_df)
 # MAGIC Train a LightGBM model on the data returned by `TrainingSet.to_df`, then log the model with `FeatureStoreClient.log_model`. The model will be packaged with feature metadata.
 
 # COMMAND ----------
-# DBTITLE 1, Train model
 
+# DBTITLE 1, Train model
 from sklearn.model_selection import train_test_split
 from mlflow.tracking import MlflowClient
 import lightgbm as lgb
@@ -240,8 +241,8 @@ num_rounds = 100
 model = lgb.train(param, train_lgb_dataset, num_rounds)
 
 # COMMAND ----------
-# DBTITLE 1, Log model and return output.
 
+# DBTITLE 1, Log model and return output.
 # Log the trained model with MLflow and package it with feature lookup information.
 fs.log_model(
     model,
